d4f25d215375eb1c4479fea6f3cb62c556c12eda
Kimbrene Kakande <kimbrenekakande@gmail.com>
2026-06-22 20:10:22 +0300

    editor content extraction

--- apps/web/components/editor.tsx
+++ apps/web/components/editor.tsx
@@ -1,8 +1,12 @@
 "use client"
 
-import { EmailEditor } from "@react-email/editor"
+import { EmailEditor, EmailEditorRef } from "@react-email/editor"
+import { composeReactEmail } from "@react-email/editor/core"
 import { extendTheme } from "@react-email/editor/plugins"
+import { Inspector } from "@react-email/editor/ui"
 import "@react-email/editor/themes/default.css"
+import { Button } from "./ui/button"
+import { useRef } from "react"
 
 
 const customTheme = extendTheme("basic", {
@@ -21,12 +25,43 @@ const customTheme = extendTheme("basic", {
 }
 )
 
-const Editor = ({ content }: { content : string }) => {
-  return (
-    <div className="flex items-center justify-center w-full h-full">
-      <EmailEditor content={content} theme={customTheme} />
+const Editor = ({ content }: { content: string }) => {
+  const editorRef = useRef<EmailEditorRef>(null)
+  
+  const handleSend = async () => {
+    if (!editorRef.current?.editor) return;
+    
+    const { text, html } = await composeReactEmail({
+      editor : editorRef.current.editor
+    })
+
+    console.log(html)
+  }
+
+  const sidebar = () => {
+    return (
+      <Inspector.Root className="w-60 shrink-0 border flex flex-col overflow-y-auto pt-8 p-4 ">
+        <Inspector.Document />
+        <Inspector.Node />
+        <Inspector.Text />
+      </Inspector.Root>
+    )
+  }
+  
+  return (  
+    <div>
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        <EmailEditor
+          content={content}
+          theme={customTheme}
+          ref={editorRef}
+          // className="flex-1 max-w-0 h-full overflow-y-auto"
+        />
+      </div>
+      <Button onClick={handleSend}> Send Email </Button>
     </div>
+    
   )
-}
+} 
 
 export default Editor;
