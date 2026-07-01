9cd6fff69a3c329458812743a794be2ad0b45a27
Kimbrene Kakande <kimbrenekakande@gmail.com>
2026-07-01 10:53:59 +0300

    typo in email update query

--- apps/web/app/api/send/[id]/route.ts
+++ apps/web/app/api/send/[id]/route.ts
@@ -50,7 +50,7 @@ export async function POST(
     if (updatedBody) {
       await prisma.email.update({
         where: { id: emailId },
-        data: { body: updatedBody },.
+        data: { body: updatedBody },
       });
     }
 
